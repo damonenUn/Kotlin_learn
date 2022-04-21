09b12a146c78601e4655d6f6e3511e5ea93abf23
_crabok <85691324+rondiak-un@users.noreply.github.com>
2022-04-21 11:46:56 +0300

    Create paintinh(class).kt

---
+++ paintinh(class).kt
@@ -0,0 +1,14 @@
+class painting {
+
+    var paintings = 2
+    var leigth = 5
+    var weight = 10
+
+    fun paint() {
+        println("painting the picture....")
+    }
+
+    fun colors () {
+
+    }
+}
