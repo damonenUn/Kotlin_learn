46d68e5a7eb120dd44c18dda69e0b42370a4c0e7
_crabok <85691324+rondiak-un@users.noreply.github.com>
2022-04-23 10:02:53 +0300

    Create painting(class)v2.kt

---
+++ painting(class)v2.kt
@@ -0,0 +1,9 @@
+fun main () {
+    var picture = painting(5, 10.0, 7.0)
+    var picture1 = painting(10, 20.0, 14.0)
+    var picture2 = painting()
+    var picture3 = painting(leigth = 5.0, weight = 6.0, paintings = 2)
+    var picture4 = painting(7)
+
+    println(picture2.paintings)
+}
