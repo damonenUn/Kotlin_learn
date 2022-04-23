4f9d8f844e951d7a7d4700e4d51afe496045f345
_crabok <85691324+rondiak-un@users.noreply.github.com>
2022-04-23 10:03:42 +0300

    Create classv2.kt

---
+++ classv2.kt
@@ -0,0 +1,15 @@
+class painting (var paintings: Int = 4, var leigth: Double = 5.0, var weight: Double = 7.0) {
+
+    fun paint() {
+        println("painting the picture....")
+    }
+
+    fun colors () {
+
+    }
+
+    fun painter (): Double {
+        return leigth * weight
+    }
+
+}
